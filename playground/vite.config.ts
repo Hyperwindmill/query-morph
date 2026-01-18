25e515f8145ae8e4830b1c9aa0f18e4c64e8f89f
Hyperwindmill <48651572+Hyperwindmill@users.noreply.github.com>
2026-01-18 22:16:42 +0100

    fixed unnecessary

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'js-beautify': path.resolve(__dirname, 'node_modules/js-beautify/js/lib/beautify.js'),
-      'chevrotain': path.resolve(__dirname, 'node_modules/chevrotain/lib/chevrotain.mjs'),
-      'fast-xml-parser': path.resolve(__dirname, 'node_modules/fast-xml-parser/src/fxp.js')
-    },
-  },
-})
+});
