df508525a80c7e0f2888c2cf5fdbf82f59dba4ca
Hyperwindmill <48651572+Hyperwindmill@users.noreply.github.com>
2026-01-25 00:46:49 +0100

    trying to fix broken pages playground

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,31 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "MorphQLPlayground",
-      fileName: (format) => `index.${format}.js`,
-      formats: ["es", "cjs"],
-    },
-    rollupOptions: {
-      external: [
-        "react",
-        "react-dom",
-        "@morphql/core",
-        "@morphql/language-definitions",
-      ],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          "@morphql/core": "MorphQLCore",
-        },
-      },
-    },
-  },
 });
